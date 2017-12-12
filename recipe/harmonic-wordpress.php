30d3d1701ccb1f43233f81029cd7cfc597597579
Craig Harman <craig@harmonicnewmedia.com>
2017-12-12 12:12:41 +0800

    Added optional gulp configuration to harmonic recipe

--- recipe/harmonic-wordpress.php
+++ recipe/harmonic-wordpress.php
@@ -12,30 +12,6 @@ require_once __DIR__ . '/harmonic.php';
 
 set('shared_files', ['wp-config.php', '.env']);
 
-desc('Harmonic Tasks');
-task('harmonic:setup', function () {
-    echo "Running project configuration (node, gulp, compilation etc)";
-    run("cd {{release_path}} && npm install");
-    run("cd {{release_path}} && node_modules/gulp/bin/gulp.js build");
-
-    $stage = null;
-    if (input()->hasArgument('stage')) {
-        $stage = input()->getArgument('stage');
-    }
-    $gulp = false;
-    if (input()->hasArgument('gulp')) {
-        $gulp = input()->getArgument('gulp');
-        if (strtolower($gulp) == 'true') $gulp = true;
-    }
-    if ('production' == $stage && $gulp) {
-        run("cd {{release_path}} && node_modules/gulp/bin/gulp.js --production");
-    } elseif ($gulp) {
-        run("cd {{release_path}} && node_modules/gulp/bin/gulp.js");
-    }
-
-    echo "Project successfully configured!";
-});
-
 task('deploy', [
     'deploy:prepare',
     'deploy:lock',
@@ -47,6 +23,7 @@ task('deploy', [
     'deploy:clear_paths',
     'deploy:symlink',
     'harmonic:setup',
+    'harmonic:gulp',
     'deploy:unlock',
     'cleanup'
 ])->desc('Deploy your project');
--- recipe/harmonic.php
+++ recipe/harmonic.php
@@ -10,14 +10,42 @@ require_once __DIR__ . '/common.php';
 
 set('default_stage', 'production');
 set('ssh_multiplexing', true);
-set('clear_paths', [ '.git', 'deploy.php', 'bitbucket-pipelines.yml' ]);
+set('clear_paths', ['.git', 'deploy.php', 'bitbucket-pipelines.yml']);
 set('shared_files', ['.env']);
 
-desc('Harmonic Tasks');
-task('harmonic:php7.1-reload', function() {
+desc('Reload PHP');
+task('harmonic:php7.1-reload', function () {
     run("sudo service php7.1-fpm reload");
 });
 
+desc('Harmonic Tasks');
+task('harmonic:setup', function () {
+    echo "Running project configuration (node, compilation etc)";
+    run("cd {{release_path}} && npm install");
+
+    echo "Project successfully configured!";
+});
+
+desc('Run Gulp (if requested)');
+task('harmonic:gulp', function () {
+    $gulp = get('gulp', false);
+    if ($gulp) {
+        run("cd {{release_path}} && node_modules/gulp/bin/gulp.js build");
+        $stage = null;
+        if (input()->hasArgument('stage')) {
+            $stage = input()->getArgument('stage');
+        }
+
+        if ('production' == $stage) {
+            run("cd {{release_path}} && node_modules/gulp/bin/gulp.js --production");
+        } elseif ($gulp) {
+            run("cd {{release_path}} && node_modules/gulp/bin/gulp.js");
+        }
+    }
+});
+
+after('deploy:symlink', 'harmonic:setup');
+after('harmonic:setup', 'harmonic:gulp');
 after('deploy:vendors', 'deploy:clear_paths');
 after('cleanup', 'harmonic:php7.1-reload');
 after('deploy:failed', 'deploy:unlock');
